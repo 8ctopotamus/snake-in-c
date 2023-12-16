4284bc82a2509995845c0cec52e5e7606bb7215c
8ctopotamus <zylo.codes@gmail.com>
2023-12-16 07:44:38 -0600

    eating food

--- snake.c
+++ snake.c
@@ -6,15 +6,18 @@ void main() {
 	WINDOW* win = initscr();
 	keypad(win, true);
 	nodelay(win, true);
-	
+	noecho();
+	curse_set(0);
+
 	int posX = 0;
 	int posY = 0;
-	int foodX = rand() % 20;
-	int foodY = rand() % 20;
 	int dirX = 1;
 	int dirY = 0;
-
+	int foodX = rand() % 20;
+	int foodY = rand() % 20;
+	
 	while(true) {
+		// keyboard input
 		int pressed = wgetch(win);
 		
 		if (pressed == KEY_LEFT) {
@@ -33,12 +36,23 @@ void main() {
 			dirX = 0;
 			dirY = 1;
 		}
+		
+		// movement
 		posX += dirX;
 		posY += dirY;
 		erase();
+		
+		// draw
 		mvaddstr(posY, posX, "*");
 		mvaddstr(foodY, foodX, "&");
+		
 		usleep(100000);
+
+		// eating food
+		if (foodX == posX && foodY == posY) {
+			foodX = rand() % 20;
+			foodY = rand() % 20;
+		}
 	}
 
 	endwin();
